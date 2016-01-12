66d3fee6264dbbbe6cd0f0557f36d7c49177f2fc
Devin Ireland <me@devinireland.com>
2016-01-12 13:37:52 -0700

    Added shared assembly info for properties that will be set across all projects in the solution

---
+++ TravelExpertsTerm2/SharedAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Resources;
+using System.Reflection;
+
+/*  Shared General Information  */
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyConfiguration("Production")]
+#endif
+[assembly: AssemblyCompany("OOSD Term 2 Project, Team 5")]
+[assembly: AssemblyProduct("Travel Experts Management System")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyCulture("")]
