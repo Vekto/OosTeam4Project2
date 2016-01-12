748ca8df6a6f119ac7cdf443b97c58a5e06ee090
Devin Ireland <me@devinireland.com>
2016-01-12 13:37:52 -0700

    Added shared assembly info for properties that will be set across all projects in the solution

---
+++ TravelExpertsTerm2/TravelDatabase/Database.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace TravelDatabase
+{
+    /// <summary>
+    ///     Global database functions like creating connections.
+    /// </summary>
+    [Chad]
+    [PublicAPI]
+    public static class Database
+    {
+        [Devin] public const string ConnectionStringFilePath = @"ConnectionString.txt";
+
+        /// <summary>
+        ///     Connection string used by all the database functions. Defaults to the first line
+        ///     in the file <see cref="ConnectionStringFilePath" />, or null if the file doesn't exist.
+        /// </summary>
+        [Devin]
+        [CanBeNull]
+        public static string ConnectionString { get; set; } =
+            File.Exists(ConnectionStringFilePath)
+                ? File.ReadLines(ConnectionStringFilePath).FirstOrDefault()
+                : null;
+    }
+}
